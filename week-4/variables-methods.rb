935b5fbdf14bbfbdf550218d8d849e049ffaf285
Mo Amin <m.alamin92@gmail.com>
2015-11-10 14:02:39 -0500

    first pine challange complete

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,8 @@
+puts "Hello there, what is your first name?"
+first_name = gets.chomp
+puts "What is your middle name?"
+middle_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Hi " + first_name + " " + middle_name + " " + last_name + "! Nice to meet you!"
