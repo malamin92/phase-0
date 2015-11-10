6a9461b8775316649581c843b19d651b0e48f28d
Mo Amin <m.alamin92@gmail.com>
2015-11-10 15:14:13 -0500

    finish simple substring

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,21 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: Mira].
+
+def welcome(address)
+	address_array = address.split(" ")
+	if address.include?("CA")
+		return "Welcome to California"
+	else
+		return "You should move to California"
+	end
+end
+
+
+# Create a method called welcome that accepts an address as a single string.
+
+# It returns "Welcome to California" if the address includes "CA" and "You should move to California" otherwise.
+
+# (This is an old joke from back in the day when Dev Bootcamp was only in San Francisco. Don't worry, you don't have to move here. There isn't enough water for everyone as it is!)
+
+# # Your Solution Below
