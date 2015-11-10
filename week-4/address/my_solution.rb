9465c6c46fb85cff4f3902db746dd9a3549540a9
Mo Amin <m.alamin92@gmail.com>
2015-11-10 16:27:30 -0500

    finish address

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,20 @@
+# Format an Address
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def make_address(street, city, state, zip)
+	return "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+end
+
+
+# Create a method called make_address that accepts parameters for the street, city , state, and zip and returns a single address string.
+
+# For example if we call the method as follows:
+
+# make_address("633 Folsom St.", "San Francisco", "CA", "94107")
+# The return value would be
+
+# "You live at 633 Folsom St., in the beautiful city of San Francisco, CA. Your zip is 94107."
--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,3 +1,5 @@
+# First Pine Challenge:
+
 puts "Hello there, what is your first name?"
 first_name = gets.chomp
 puts "What is your middle name?"
@@ -5,4 +7,11 @@ middle_name = gets.chomp
 puts "What is your last name?"
 last_name = gets.chomp
 
-puts "Hi " + first_name + " " + middle_name + " " + last_name + "! Nice to meet you!"
+# puts "Hi " + first_name + " " + middle_name + " " + last_name + "! Nice to meet you!"
+
+# Second Pine Challenge:
+
+puts "What is your favorite number?"
+favorite_number = gets.chomp
+puts "Have you considered #{favorite_number.to_i + 1} and your favorite number? It's bigger and better!"
+
