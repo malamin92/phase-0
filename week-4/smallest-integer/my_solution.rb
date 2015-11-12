9dbeac57dc0fdb20fbeaee65546223fd61689539
Mo Amin <m.alamin92@gmail.com>
2015-11-12 05:06:03 -0500

    smallest integer complete

---
+++ week-4/smallest-integer/my_solution.rb
@@ -0,0 +1,34 @@
+# Smallest Integer
+
+# I worked on this challenge by myself.
+
+# smallest_integer is a method that takes an array of integers as its input
+# and returns the smallest integer in the array
+#
+# +list_of_nums+ is an array of integers
+# smallest_integer(list_of_nums) should return the smallest integer in +list_of_nums+
+#
+# If +list_of_nums+ is empty the method should return nil
+
+# Your Solution Below
+def smallest_integer(list_of_nums)
+  # Your code goes here!
+  if !list_of_nums.empty?
+  	return list_of_nums.min
+  else
+  	return nil
+  end
+end
+
+# In the my_solution.rb file:
+
+# Write a method smallest_integer which takes as its input an Array of integers and returns the smallest integer in theArray.
+
+# For example:
+
+# smallest_integer([1, 2, 3])       # => 1
+# smallest_integer([0, -10, 10])    # => -10
+# smallest_integer([-10, -20, -30]) # => -30
+# If the input Array is empty smallest_integer should return nil.
+
+# For the initial solution, focus on using iteration. Avoid using any built in Ruby methods you find in the docs. Keep the solution pretty simple. 
