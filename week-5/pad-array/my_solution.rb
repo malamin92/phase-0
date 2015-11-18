cc931374e806ad37df9e172ab3d5ec7d04e236c0
Mo Amin <m.alamin92@gmail.com>
2015-11-17 21:12:53 -0500

    finish solution

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -14,6 +14,7 @@
 # 0. Pseudocode
 
 # What is the input?
+
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
@@ -21,11 +22,35 @@
 # 1. Initial Solution
 def pad!(array, min_size, value = nil) #destructive
   # Your code here
+  if array.size == min_size
+    return array
+  else
+    while min_size > array.size
+      array << value
+    end
+  end
+  return array
 end
+
+# def pad(array, min_size, value = nil) #non-destructive
+#   # Your code here
+#   new_array = array.dup
+#   if array.size == min_size
+#   	return new_array
+#   else
+#     while min_size > new_array.size
+#       new_array << value
+#     end
+#   end
+#   return new_array
+# end
 
 def pad(array, min_size, value = nil) #non-destructive
   # Your code here
+  pad!(array.dup, min_size, value)
 end
+
+p pad([1,2,3], 5)
 
 
 # 3. Refactored Solution
