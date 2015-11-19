01ca15d6f45a11166936fd8fc2b582571bdd8a14
Mo Amin <m.alamin92@gmail.com>
2015-11-19 01:59:26 -0500

    add name

--- week-5/group-reserach-methods/my_solution.rb
+++ week-5/group-reserach-methods/my_solution.rb
@@ -5,7 +5,7 @@
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
-# Person 1's solution
+# Person 1's solution - Mohammad Amin
 def my_array_finding_method(source, thing_to_find)
   source.select{|item| item.to_s.include?(thing_to_find)}
 end
