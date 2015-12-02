4615de42e22ba6433f53990525a8d84dd783ad1e
Mo Amin <m.alamin92@gmail.com>
2015-12-01 21:38:29 -0500

    finish intro

---
+++ week-7/eloquent.js
@@ -0,0 +1,47 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+/*
+Questions:
+Introduction:
+Did you learn anything new about JavaScript or programming in general?
+	
+	I learned some interesting facts about programming in general as well as JS. For example, the early
+	computers were just running binary operations. I didn't know that Netscape was the first browser to 
+	include JS. I didn't know ECMAScript and JavaScript meant the same thing. It's cool that JavaScript
+	is also used in databases like MongoDB, that's something new to me.
+
+Are there any concepts you feel uncomfortable with?
+
+	There are no cocepts that I feel uncomfortable with.
+
+Identify two similarities and two differences between JavaScript and Ruby syntax with regard to 
+numbers, arithmetic, strings, booleans, and various operators.
+
+	
+
+
+
+*/
