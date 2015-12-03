05ca57be2aba9517ac7f9c646645b9e668bddad4
Mo Amin <m.alamin92@gmail.com>
2015-12-03 15:09:30 -0500

    finish 7.4

---
+++ week-7/manipulating_objects.js
@@ -0,0 +1,155 @@
+ // Manipulating JavaScript Objects
+
+// I worked on this challenge: [by myself, with: ]
+
+// There is a section below where you will write your code.
+
+// DO NOT ALTER THIS OBJECT BY ADDING ANYTHING WITHIN THE CURLY BRACES!
+var terah = {
+  name: "Terah",
+  age: 32,
+  height: 66,
+  weight: 130,
+  hairColor: "brown",
+  eyeColor: "brown"
+}
+// __________________________________________
+// Write your code below.
+var adam = {
+  name: "Adam"
+
+}
+terah.spouse = adam;
+terah.weight = 125;
+delete terah.eyeColor;
+adam.spouse = terah;
+terah.children = {};
+var carson = { name: "Carson" }
+terah.children.carson = carson;
+var carter = {name: "Carter" }
+terah.children.carter = carter;
+var colton =  {name: "Colton"}
+terah.children.colton = colton;
+adam.children = terah.children;
+
+
+// __________________________________________
+// Reflection: Use the reflection guidelines
+/*
+What tests did you have trouble passing? What did you do to make it pass? Why did that work?
+  
+  I had no problems making any of the tests pass. Objects in JS are like hashes in Ruby
+  and it helps to visualize them in that way.  
+
+How difficult was it to add and delete properties outside of the object itself?
+
+  It eas not defficult. Essentially, it was just a matter of using the delete keyword
+  and refering that to the property we are trying to delete. The syntax is easy to 
+  understand and seems to make sense altogether.  
+
+What did you learn about manipulating objects in this challenge?
+
+  I learned that you can have objects inside objects, kind of like how you can have hashes
+  inside of hashes. Also I think objects are very similar to Ruby hashes except they are more
+  powerful because they can hold functions as well. I guess they are a mixture of classes
+  and hashes from Ruby, but not completely.
+
+*/
+
+// __________________________________________
+// Driver Code:  Do not alter code below this line.
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (adam instanceof Object),
+  "The value of adam should be an Object.",
+  "1. "
+)
+
+assert(
+  (adam.name === "Adam"),
+  "The value of the adam name property should be 'Adam'.",
+  "2. "
+)
+
+assert(
+  terah.spouse === adam,
+  "terah should have a spouse property with the value of the object adam.",
+  "3. "
+)
+
+assert(
+  terah.weight === 125,
+  "The terah weight property should be 125.",
+  "4. "
+)
+
+assert(
+  terah.eyeColor === undefined || null,
+  "The terah eyeColor property should be deleted.",
+  "5. "
+)
+
+assert(
+  terah.spouse.spouse === terah,
+  "Terah's spouse's spouse property should refer back to the terah object.",
+  "6. "
+)
+
+assert(
+  (terah.children instanceof Object),
+  "The value of the terah children property should be defined as an Object.",
+  "7. "
+)
+
+assert(
+  (terah.children.carson instanceof Object),
+  "carson should be defined as an object and assigned as a child of Terah",
+  "8. "
+  )
+
+assert(
+  terah.children.carson.name === "Carson",
+  "Terah's children should include an object called carson which has a name property equal to 'Carson'.",
+  "9. "
+)
+
+assert(
+  (terah.children.carter instanceof Object),
+  "carter should be defined as an object and assigned as a child of Terah",
+  "10. "
+  )
+
+assert(
+  terah.children.carter.name === "Carter",
+  "Terah's children should include an object called carter which has a name property equal to 'Carter'.",
+  "11. "
+)
+
+assert(
+  (terah.children.colton instanceof Object),
+  "colton should be defined as an object and assigned as a child of Terah",
+  "12. "
+  )
+
+assert(
+  terah.children.colton.name === "Colton",
+  "Terah's children should include an object called colton which has a name property equal to 'Colton'.",
+  "13. "
+)
+
+assert(
+  adam.children === terah.children,
+  "The value of the adam children property should be equal to the value of the terah children property",
+  "14. "
+)
+
+console.log("\nHere is your final terah object:")
+console.log(terah)
