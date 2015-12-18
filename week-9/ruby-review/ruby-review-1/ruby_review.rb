5503bedb8ea8d2a407ae251fb8b03cb72f52cff2
Mo Amin <m.alamin92@gmail.com>
2015-12-18 00:24:03 -0500

    finish pseudocode

---
+++ week-9/ruby-review/ruby-review-1/ruby_review.rb
@@ -0,0 +1,31 @@
+# Reverse Words
+
+
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+=begin
+1. define a method to reverse words in a given sentence
+	input: a string
+	output: a string where the words are reversed 
+2. take the input string and split the words
+3. iterate over each word and reverse it
+4. join the new array with a space between each word
+=end
+
+
+# Initial Solution
+def reverse_words(sentence)
+	
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+# Reflection
